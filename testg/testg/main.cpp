262efab051ba0d3e1f39242c7a392e6d3283d384
Ting-Jung, Hsaio <r09527a04@ntu.edu.tw>
2021-12-23 19:41:07 +0800

    practice

---
+++ testg/testg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testg
+//
+//  Created by 蕭廷融 on 2021/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
